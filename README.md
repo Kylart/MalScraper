0040c86f1de4ef12f3a45e98a506945d9c1a265e
Kylart <garnier@et.esiea.fr>
2016-12-07 15:50:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# _MalScraper_
+This will be a npm module getting information about anime on MyAnimeList.net.
+
+At the moment, MalScraper is not ready to be used as a NPM module (TODO).
+
+At the moment, _MalScraper_ is able to:
+* Gather information about all the anime being releases in a season
+
+_MalScraper_ is being developed mainly for [_KawAnime_](https://github.com/Kylart/KawAnime) but anyone can use it for
+ its own purpose.
+
+Any contribution would be welcomed :simple_smile:
+
+## Installation
+TODO
+    
+    `npm install <TO DEFINE>`
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request.
+
+## TODOs
+* Make it into a NPM Module
+* More features
+
+## License
+MIT License
+
+Copyright (c) Kylart
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
---
+++ index.js
@@ -0,0 +1,202 @@
+/**
+ * Created by Kylart on 07/12/2016.
+ */
+
+const fs = require('fs')
+const express = require('express')
+const req = require('req-fast')
+const cheerio = require('cheerio')
+
+const SEASON_URL_PATH = 'https://myanimelist.net/anime/season/'
+
+// For performance
+let t1, t2
+
+let animeJSON = {
+    titles: [],
+    genres: [],
+    images: [],
+    scores: [],
+    synopsis: [],
+    producers: [],
+    releaseDates: [],
+    types: [],
+    nbEps: [],
+    fromType: [],
+    stats: {
+        TVNumber: 0,
+        ONANumber: 0,
+        OVANumber: 0,
+        MovieNumber: 0,
+        SpecialNumber: 0
+    }
+}
+
+let loadTitles = ($) => {
+    let tmp = $('.title-text').text().split('\n        ')
+
+    tmp.shift()     // Getting rid of an empty element at the beginning.
+
+    tmp.forEach( (elem) => {
+        animeJSON.titles.push(elem.slice(0, -7))     // Loading the right name
+    })
+}
+
+let loadProducer = ($) => {
+    $('.producer').each( function () {
+        animeJSON.producers.push($(this).text())
+    })
+}
+
+let loadNbEpisodes = ($) => {
+    let tmp = []
+
+    $('.eps').each( function () {
+        tmp.push($(this).text().split(' '))
+    })
+
+    // Only 16th and 17th element are interesting
+    for (let i = 0; i < tmp.length; ++i)
+    {
+        tmp[i] = tmp[i].slice(16, 18)
+    }
+
+    tmp.forEach( (elem) => {
+        animeJSON.nbEps.push(elem.join(' ').slice(0, -1))
+    })
+}
+
+let loadGenres = ($) => {
+    $('.genres-inner').each(function () {
+        let tmp = $(this).text().split('\n        ')
+        tmp.shift()
+
+        for (let i = 0; i < tmp.length; ++i)
+        {
+            tmp[i] = tmp[i].slice(0, -7)
+        }
+
+        animeJSON.genres.push(tmp)
+    })
+}
+
+let loadSynopsis = ($) => {
+    $('.synopsis').each( function () {
+        animeJSON.synopsis.push($(this).text().slice(5, -8))
+    })
+}
+
+let loadImages = ($) => {
+    $('.image').each( function () {
+        animeJSON.images.push($(this).attr('data-bg'))
+    })
+}
+
+let loadScores = ($) => {
+    $('.score').each( function () {
+        animeJSON.scores.push($(this).text().slice(9, 13))
+    })
+}
+
+let loadReleaseDates = ($) => {
+    $('.remain-time').each(function () {
+        animeJSON.releaseDates.push($(this).text().slice(19, -27))
+    })
+}
+
+let loadTypes = ($) => {
+    let stats = {
+        TVNumber: 0,        // js-seasonal-anime-list-key-1
+        ONANumber: 0,       // js-seasonal-anime-list-key-5
+        OVANumber: 0,       // js-seasonal-anime-list-key-2
+        MovieNumber: 0,     // js-seasonal-anime-list-key-3
+        SpecialNumber: 0    // js-seasonal-anime-list-key-4
+    }
+
+    $('.js-seasonal-anime-list-key-1').find('div.seasonal-anime').each(function () {
+        ++stats.TVNumber
+        animeJSON.types.push('TV')
+    })
+
+    $('.js-seasonal-anime-list-key-5').find('div.seasonal-anime').each(function () {
+        ++stats.ONANumber
+        animeJSON.types.push('ONA')
+    })
+
+    $('.js-seasonal-anime-list-key-2').find('div.seasonal-anime').each(function () {
+        ++stats.OVANumber
+        animeJSON.types.push('OVA')
+    })
+
+    $('.js-seasonal-anime-list-key-3').find('div.seasonal-anime').each(function () {
+        ++stats.MovieNumber
+        animeJSON.types.push('Movie')
+    })
+
+    $('.js-seasonal-anime-list-key-4').find('div.seasonal-anime').each(function () {
+        ++stats.SpecialNumber
+        animeJSON.types.push('Special')
+    })
+
+    animeJSON.stats = stats
+}
+
+let loadFromType = ($) => {
+    $('.source').each(function () {
+        animeJSON.fromType.push($(this).text())
+    })
+}
+
+let loadJSON = ($) => {
+    loadTitles($)
+    loadProducer($)
+    loadNbEpisodes($)
+    loadGenres($)
+    loadSynopsis($)
+    loadImages($)
+    loadScores($)
+    loadReleaseDates($)
+    loadTypes($)
+    loadFromType($)
+}
+
+let getSeason = (year, season) => {
+    t1 = Date.now()
+
+    const url = `${SEASON_URL_PATH}${year}/${season}`
+
+    req(url, (err, response) => {
+        if (err) throw err
+
+        const html = response.body
+        const $ = cheerio.load(html)
+
+        loadJSON($)
+
+        // Make array to return
+        let result = []
+
+        for (let i = 0; i < animeJSON.titles.length; ++i)
+        {
+            result.push({
+                title: animeJSON.titles[i],
+                genres: animeJSON.genres[i],
+                image: animeJSON.images[i],
+                scores: animeJSON.scores[i],
+                synopsis: animeJSON.synopsis[i],
+                producers: animeJSON.producers[i],
+                releaseDates: animeJSON.releaseDates[i],
+                type: animeJSON.types[i],
+                nbEp: animeJSON.nbEps[i],
+                fromType: animeJSON.fromType[i]
+            })
+        }
+
+        console.log(result[0])
+
+        t2 = Date.now()
+        console.log(`Seasonal information gathered in: ${(t2 - t1) / 1000}s.`)
+    })
+}
+
+getSeason(2017, 'winter')
