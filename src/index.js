fa0376ec5550a7faea8df3bc4ec81098f698da86
Kylart <Kylart.dev@gmail.com>
2017-10-30 22:43:53 +0100

    removed spread operator for es5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const officialApi = require('./officialApi')
 const getSeason = require('./seasons.js')
 const getNewsNoDetails = require('./news')
-const info = require('./info')
-const watchList = require('./watchList')
+const {getInfoFromName, getInfoFromURL, getResultsFromSearch} = require('./info')
+const {getWatchListFromUser} = require('./watchList')
 
 module.exports = {
   officialApi,
   getSeason,
   getNewsNoDetails,
-  ...info,
-  ...watchList
+  getInfoFromName,
+  getInfoFromURL,
+  getResultsFromSearch,
+  getWatchListFromUser
 }
