f006ae0ff629a456bcff3283dcd53317f73c8754
Latsuj <justal.kevin@gmail.com>
2020-08-19 23:30:43 +0800

    [FEATURE] Adding the tests and the function for reading the Mal Object

--- src/reviews.js
+++ src/reviews.js
@@ -1,15 +1,53 @@
 const axios = require('axios');
 
-const getReviews = (offset = 0, res = []) => {
+const getReviewsFromName = (name) => {
   return new Promise((resolve, reject) => {
-    axios.get("https://myanimelist.net/anime/1735/Naruto__Shippuuden/reviews")
-    .then(({ data }) => {
-        console.log(data)
+    getResultsFromSearch(name)
+      .then((items) => {
+        const { url } = items[0]
+
+        searchPage(`${encodeURI(url)}/reviews`)
+          .then((data) => resolve(data))
+          .catch(/* istanbul ignore next */(err) => reject(err))
       })
       .catch(/* istanbul ignore next */(err) => reject(err))
   })
 }
 
+const getReviewsFromNameAndId = (id, name) => {
+  return new Promise((resolve, reject) => {
+    searchPage(`${BASE_URI}${id}/${encodeURI(name)}/reviews`)
+      .then((data) => resolve(data))
+      .catch(/* istanbul ignore next */(err) => reject(err))
+  })
+}
+
+const getReviewsList = (obj) => {
+  return new Promise((resolve, reject) => {
+    if (!obj) {
+      reject(new Error('[Mal-Scraper]: No id nor name received.'))
+      return
+    }
+
+    if (typeof obj === 'object' && !obj[0]) {
+      const { id, name } = obj
+
+      if (!id || !name || isNaN(+id) || typeof name !== 'string') {
+        reject(new Error('[Mal-Scraper]: Malformed input. ID or name is malformed or missing.'))
+        return
+      }
+
+      getReviewsFromNameAndId(id, name)
+        .then((data) => resolve(data))
+        .catch(/* istanbul ignore next */(err) => reject(err))
+    } else {
+      getReviewsFromName(obj)
+        .then((data) => resolve(data))
+        .catch(/* istanbul ignore next */(err) => reject(err))
+    }
+  })
+}
+
 module.exports = {
-  getReviews
+  getReviewsList
 }
