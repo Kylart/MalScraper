c8ce7b2fb6d829b0e88ab1f03bfab187a3a1bd80
Latsuj <justal.kevin@gmail.com>
2020-09-04 20:32:59 +0800

    [LINTER] Fixing the error of the linter

--- src/reviews.js
+++ src/reviews.js
@@ -1,17 +1,17 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require('axios')
+const cheerio = require('cheerio')
 const { getResultsFromSearch } = require('./info.js')
 
 const BASE_URI = 'https://myanimelist.net/anime/'
-const NUMBER_REVIEWS_BY_PAGE = 20;
-const INITIAL_FIRST_PAGE_REVIEW = 1;
+const NUMBER_REVIEWS_BY_PAGE = 20
+const INITIAL_FIRST_PAGE_REVIEW = 1
 
 const malDateToJsDate = (malDate) => {
-	return new Date(malDate);
+  return new Date(malDate)
 }
 
 const malNumberToJsNumber = (malNumber) => {
-	return malNumber ? Number(malNumber) : 0;
+  return malNumber ? Number(malNumber) : 0
 }
 
 const parsePage = ($) => {
@@ -19,16 +19,16 @@ const parsePage = ($) => {
   const result = []
 
   items.each(function (elem) {
-    const notes = $(this).find('.spaceit.pt8 div');
-    const review_more = $(this).find('.spaceit.pt8 span');
+    const notes = $(this).find('.spaceit.pt8 div')
+    const reviewMore = $(this).find('.spaceit.pt8 span')
     // For presenting the review only without the notes
-    $(this).find('.spaceit.pt8 div').remove();
-    $(this).find('.spaceit.pt8 span').remove();
-    $(this).find('.spaceit.pt8 a.js-toggle-review-button').remove();
+    $(this).find('.spaceit.pt8 div').remove()
+    $(this).find('.spaceit.pt8 span').remove()
+    $(this).find('.spaceit.pt8 a.js-toggle-review-button').remove()
 
     result.push({
-      author: $($(this).find('.spaceit td:nth-child(2) a')["0"]).text().trim(),
-      date: malDateToJsDate($($(this).find('.spaceit .mb8 div')["0"]).text().trim()),
+      author: $($(this).find('.spaceit td:nth-child(2) a')['0']).text().trim(),
+      date: malDateToJsDate($($(this).find('.spaceit .mb8 div')['0']).text().trim()),
       seen: $(this).find('.spaceit .mb8 .lightLink').text().trim(),
       note_overall: malNumberToJsNumber($(notes).find('tr:nth-child(1) td:nth-child(2)').text().trim()),
       note_story: malNumberToJsNumber($(notes).find('tr:nth-child(2) td:nth-child(2)').text().trim()),
@@ -36,7 +36,7 @@ const parsePage = ($) => {
       note_sound: malNumberToJsNumber($(notes).find('tr:nth-child(4) td:nth-child(2)').text().trim()),
       note_character: malNumberToJsNumber($(notes).find('tr:nth-child(5) td:nth-child(2)').text().trim()),
       note_enjoyment: malNumberToJsNumber($(notes).find('tr:nth-child(6) td:nth-child(2)').text().trim()),
-      review: $(this).find('.spaceit.pt8').text().trim() + $(review_more).text().trim()
+      review: $(this).find('.spaceit.pt8').text().trim() + $(reviewMore).text().trim()
     })
   })
 
@@ -49,46 +49,42 @@ const searchPage = (url, limit, skip, p, res = []) => {
       params: {
         p
       }
-    })
-      .then(({ data }) => {
-        const $ = cheerio.load(data)
+    }).then(({ data }) => {
+      const $ = cheerio.load(data)
 
-        const tmpRes = parsePage($)
-        res = res.concat(tmpRes)
+      const tmpRes = parsePage($)
+      res = res.concat(tmpRes)
 
-		if (skip !== 0) {
-			res.splice(0, skip)
-			skip = 0
-		}
+      if (skip !== 0) {
+        res.splice(0, skip)
+        skip = 0
+      }
 
-		if (res.length <= limit) {
-			p++;
-			searchPage(url, limit, skip, p, res)
-			  .then((data) => resolve(data))
-			  .catch(/* istanbul ignore next */(err) => reject(err))
-		} else {
-            if (res.length !== limit) {
-                const nbrElementToRemove = res.length - limit;
-    			res.splice(-nbrElementToRemove, nbrElementToRemove)
-            }
-		  resolve(res)
-		}
-      })
-      .catch(/* istanbul ignore next */(err) => reject(err))
+      if (res.length <= limit) {
+        p++
+        searchPage(url, limit, skip, p, res)
+          .then((data) => resolve(data))
+          .catch(/* istanbul ignore next */(err) => reject(err))
+      } else {
+        if (res.length !== limit) {
+          const nbrElementToRemove = res.length - limit
+          res.splice(-nbrElementToRemove, nbrElementToRemove)
+        }
+        resolve(res)
+      }
+    }).catch(/* istanbul ignore next */(err) => reject(err))
   })
 }
 
 const getReviewsFromName = (name, limit, skip, p) => {
   return new Promise((resolve, reject) => {
-    getResultsFromSearch(name)
-      .then((items) => {
-        const { url } = items[0]
+    getResultsFromSearch(name).then((items) => {
+      const { url } = items[0]
 
-        searchPage(`${encodeURI(url)}/reviews`, limit, skip, p)
-          .then((data) => resolve(data))
-          .catch(/* istanbul ignore next */(err) => reject(err))
-      })
-      .catch(/* istanbul ignore next */(err) => reject(err))
+      searchPage(`${encodeURI(url)}/reviews`, limit, skip, p)
+        .then((data) => resolve(data))
+        .catch(/* istanbul ignore next */(err) => reject(err))
+    }).catch(/* istanbul ignore next */(err) => reject(err))
   })
 }
 
@@ -114,12 +110,10 @@ const getReviewsList = (obj) => {
       return
     }
 
-
+    let p = INITIAL_FIRST_PAGE_REVIEW
     if (skip !== 0) {
-	  p = Math.floor(skip / NUMBER_REVIEWS_BY_PAGE) + 1;
-	  skip = Math.max(0, skip - ((p - 1) * NUMBER_REVIEWS_BY_PAGE));
-    } else {
-	  p = INITIAL_FIRST_PAGE_REVIEW;
+      p = Math.floor(skip / NUMBER_REVIEWS_BY_PAGE) + 1
+      skip = Math.max(0, skip - ((p - 1) * NUMBER_REVIEWS_BY_PAGE))
     }
 
     if (obj.id) {
--- test/reviews.test.js
+++ test/reviews.test.js
@@ -15,60 +15,7 @@ test('getReviewsList returns the first review for Naruto Shippuuden with ID and 
     const data = await getReviewsList({
       name: NS.name,
       id: NS.id,
-	  limit: 40
-    })
-
-	t.is(typeof data, 'object')
-    t.is(data.length, 40)
-    t.is(data[0].author, 'Xyik')
-    t.is(data[0].note_overall, 6)
-    t.is(data[0].note_story, 8)
-    t.is(data[0].note_animation, 7)
-    t.is(data[0].note_sound, 6)
-    t.is(data[0].note_character, 8)
-    t.is(data[0].note_enjoyment, 7)
-	t.is(data[20].author, 'husa2004')
-    t.is(data[20].note_overall, 10)
-    t.is(data[20].note_story, 10)
-    t.is(data[20].note_animation, 9)
-    t.is(data[20].note_sound, 9)
-    t.is(data[20].note_character, 9)
-    t.is(data[20].note_enjoyment, 10)
-  } catch (e) {
-      console.log(e);
-    t.fail()
-  }
-})
-
-test('getReviewsList returns the second reviews after the first page for Naruto Shippuuden with ID and name', async t => {
-  try {
-    const data = await getReviewsList({
-      name: NS.name,
-      id: NS.id,
-	  limit: 1,
-	  skip: 21
-    })
-
-	t.is(typeof data, 'object')
-    t.is(data.length, 1)
-    t.is(data[0].author, 'Bluthut')
-    t.is(data[0].note_overall, 9)
-    t.is(data[0].note_story, 9)
-    t.is(data[0].note_animation, 8)
-    t.is(data[0].note_sound, 9)
-    t.is(data[0].note_character, 9)
-    t.is(data[0].note_enjoyment, 10)
-  } catch (e) {
-      console.log(e);
-    t.fail()
-  }
-})
-
-test('getReviewsList returns the 20 firsts review base on the name of the anime', async t => {
-  try {
-    const data = await getReviewsList({
-        name: NS.name,
-        limit: 40
+      limit: 40
     })
 
     t.is(typeof data, 'object')
@@ -80,7 +27,58 @@ test('getReviewsList returns the 20 firsts review base on the name of the anime'
     t.is(data[0].note_sound, 6)
     t.is(data[0].note_character, 8)
     t.is(data[0].note_enjoyment, 7)
-	t.is(data[20].author, 'husa2004')
+    t.is(data[20].author, 'husa2004')
+    t.is(data[20].note_overall, 10)
+    t.is(data[20].note_story, 10)
+    t.is(data[20].note_animation, 9)
+    t.is(data[20].note_sound, 9)
+    t.is(data[20].note_character, 9)
+    t.is(data[20].note_enjoyment, 10)
+  } catch (e) {
+    t.fail()
+  }
+})
+
+test('getReviewsList returns the second reviews after the first page for Naruto Shippuuden with ID and name', async t => {
+  try {
+    const data = await getReviewsList({
+      name: NS.name,
+      id: NS.id,
+      limit: 1,
+      skip: 21
+    })
+
+    t.is(typeof data, 'object')
+    t.is(data.length, 1)
+    t.is(data[0].author, 'Bluthut')
+    t.is(data[0].note_overall, 9)
+    t.is(data[0].note_story, 9)
+    t.is(data[0].note_animation, 8)
+    t.is(data[0].note_sound, 9)
+    t.is(data[0].note_character, 9)
+    t.is(data[0].note_enjoyment, 10)
+  } catch (e) {
+    t.fail()
+  }
+})
+
+test('getReviewsList returns the 20 firsts review base on the name of the anime', async t => {
+  try {
+    const data = await getReviewsList({
+      name: NS.name,
+      limit: 40
+    })
+
+    t.is(typeof data, 'object')
+    t.is(data.length, 40)
+    t.is(data[0].author, 'Xyik')
+    t.is(data[0].note_overall, 6)
+    t.is(data[0].note_story, 8)
+    t.is(data[0].note_animation, 7)
+    t.is(data[0].note_sound, 6)
+    t.is(data[0].note_character, 8)
+    t.is(data[0].note_enjoyment, 7)
+    t.is(data[20].author, 'husa2004')
     t.is(data[20].note_overall, 10)
     t.is(data[20].note_story, 10)
     t.is(data[20].note_animation, 9)
